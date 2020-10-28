59dd6873f75e07332b74712585a3786a856310f3
S0ph1ia <badalovasophia@gmail.com>
2020-10-28 17:54:43 +0000

    Made change to calc factorial + included test change

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -6,6 +6,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.aws.codestar.projecttemplates.GatewayResponse;
 import org.json.JSONObject;
 
+import java.io.BufferedWriter;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +19,21 @@ public class HelloWorldHandler implements RequestHandler<Object, Object> {
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        return new GatewayResponse(new JSONObject().put("Output", "I say - Hello World!").toString(), headers, 200);
+
+                long start = System.currentTimeMillis();
+
+
+                int numberToCalculate = 2000;
+                long fact = 0;
+
+                for (int i=1; i<=numberToCalculate; i++) {
+                    fact = fact * i;
+                }
+                long end  = System.currentTimeMillis();
+
+                long timeTaken = end-start;
+
+
+        return new GatewayResponse(new JSONObject().put("Output", "I say - Hello World!, time taken : " + timeTaken).toString(), headers, 200);
     }
 }
