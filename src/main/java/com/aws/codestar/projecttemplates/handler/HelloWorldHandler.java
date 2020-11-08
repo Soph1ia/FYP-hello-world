60db1049052e63b982b5cd6e3fbbea66c3cce722
S0ph1ia <badalovasophia@gmail.com>
2020-11-08 19:02:57 +0000

    modified helloWorldHandler

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -3,6 +3,7 @@ package com.aws.codestar.projecttemplates.handler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import com.aws.codestar.benchmarks.BenchMark;
 import com.aws.codestar.projecttemplates.GatewayResponse;
 import org.json.JSONObject;
 
@@ -17,6 +18,8 @@ public class HelloWorldHandler implements RequestHandler<Object, Object> {
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
+
+        BenchMark bm = new BenchMark();
 
         return new GatewayResponse(new JSONObject().put("Output", " Hello World " ).toString(), headers, 200);
     }
